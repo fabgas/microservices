ae337e48888cbfae07fdd7b53fd6b9ce8ba9c799
fabrice.gastineau <fabrice.gastineau@gmail.com>
2018-10-16 19:42:01 +0200

    initialisation

---
+++ produits/src/main/java/org/tutorial/produits/domain/Produit.java
@@ -0,0 +1,22 @@
+package org.tutorial.produits.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Classe entity des produits
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public final class Produit{
+
+    private Long id;
+    private String codeProduit;
+    private String libelleProduit;
+    private Double poids;
+
+    
+
+}
